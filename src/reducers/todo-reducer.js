dd51f6de1f209810c096fa8a995173ef49759fb0
Lebed, Alexander <Alexander.Lebed@team.neustar>
2017-08-08 10:37:13 +0300

    add more todos

--- src/reducers/todo-reducer.js
+++ src/reducers/todo-reducer.js
@@ -6,14 +6,17 @@ import uid from '../helpers/id-generator';
 
 const initTodos = List([
     Map({id: uid(), isDone: true, text: 'use Redux', isDeleted: false}),
-    Map({id: uid(), isDone: true,  text: 'use ImmutableJS', isDeleted: false}),
+    Map({id: uid(), isDone: true, text: 'use ImmutableJS', isDeleted: false}),
     Map({id: uid(), isDone: true, text: 'rebuild on code changes', isDeleted: false}),
     Map({id: uid(), isDone: true, text: 'use react-bootstrap', isDeleted: false}),
-    Map({id: uid(), isDone: true,  text: 'push to Github', isDeleted: false}),
-    Map({id: uid(), isDone: true,  text: 'add Router', isDeleted: false}),
+    Map({id: uid(), isDone: true, text: 'push to Github', isDeleted: false}),
+    Map({id: uid(), isDone: true, text: 'add Router', isDeleted: false}),
     Map({id: uid(), isDone: true, text: 'type checking with Flow', isDeleted: false}),
     Map({id: uid(), isDone: false, text: 'test with Jest', isDeleted: false}),
-    Map({id: uid(), isDone: false, text: 'linting with ESLint', isDeleted: false})
+    Map({id: uid(), isDone: false, text: 'linting with ESLint', isDeleted: false}),
+    Map({id: uid(), isDone: false, text: 'log in page', isDeleted: false}),
+    Map({id: uid(), isDone: false, text: 'bindActionCreators (Redux)', isDeleted: false}),
+    Map({id: uid(), isDone: false, text: 'use cookies or localStorage', isDeleted: false})
 ]);
 
 
