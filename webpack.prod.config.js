e8ddf2adfb06c2444b1a08a60d539f35097fc224
alexander.lebed <alexander.lebed@ab-soft.net>
2018-02-13 15:27:16 +0200

    prepare for Heroku webpack.prod.config.js

---
+++ webpack.prod.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+    context: __dirname,
+    entry: {
+        javascript: './src/app.js',
+        html: './index.html'
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: "[name].js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react', 'flow']
+                }
+            },
+            {
+                test: /\.js$/,
+                loaders: ['babel-loader'], // add 'eslint-loader' to lint on build
+                exclude: /node_modules/
+            },
+            {
+                test: /\.html$/,
+                loader: 'file-loader?name=[name].[ext]'
+            }
+        ]
+    }
+};
